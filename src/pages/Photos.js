a118876e50804f8c024e80408e46d9beb6919178
Brent Sandstrom <30700614+bsandstrom@users.noreply.github.com>
2022-01-02 13:19:04 -0500

    Switching to different gallery lib

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,42 +1,46 @@
-import "photoswipe/dist/photoswipe.css";
-import "photoswipe/dist/default-skin/default-skin.css";
 import "../css/Photos.css";
-import { Gallery, Item } from "react-photoswipe-gallery";
 import Template from "../components/Template";
 
-const getKittens = () => {
-  let kittens = [];
-  for (let i = 0; i < 30; i++) {
-    kittens.push(
-      <Item
-        original={`https://placekitten.com/1024/768?image=${i}`}
-        thumbnail={`https://placekitten.com/300/300?image=${i}`}
-        width="1024"
-        height="768"
-      >
-        {({ ref, open }) => (
-          <img
-            ref={ref}
-            onClick={open}
-            src={`https://placekitten.com/300/300?image=${i}`}
-            alt="cute kitten"
-          />
-        )}
-      </Item>
-    );
-  }
-  return kittens;
+import Gallery from "react-photo-gallery";
+
+const photos = [
+	{
+		src: `${process.env.PUBLIC_URL}/images/roadtrip.jpg`,
+		height: 720,
+		width: 960,
+	},
+	{
+		src: `${process.env.PUBLIC_URL}/images/snow.jpg`,
+		height: 720,
+		width: 960,
+	},
+	{
+		src: `${process.env.PUBLIC_URL}/images/firstMet.jpg`,
+		height: 720,
+		width: 960,
+	},
+	{
+		src: `${process.env.PUBLIC_URL}/images/roadtrip.jpg`,
+		height: 720,
+		width: 960,
+	},
+	{
+		src: `${process.env.PUBLIC_URL}/images/roadtrip.jpg`,
+		height: 720,
+		width: 960,
+	},
+];
+
+const openLightbox = () => {
+	console.log("open");
 };
 
 function Photos() {
-  return (
-    <Template>
-      <div id="photoContainer">
-        <Gallery>{getKittens()}</Gallery>
-      </div>
-      <footer>Website written in React by Brent</footer>
-    </Template>
-  );
+	return (
+		<Template>
+			<Gallery photos={photos} onClick={openLightbox} />
+		</Template>
+	);
 }
 
 export default Photos;
